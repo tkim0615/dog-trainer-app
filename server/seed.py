8fff1ea1dfa8a5288adead61cec81baf020f1b94
tyler kim <yoonsoo.kim11@gmail.com>
2024-02-09 12:23:32 -0500

    created config.py;

--- server/seed.py
+++ server/seed.py
@@ -1,71 +1,17 @@
+#!/usr/bin/env python3
+
+# Standard library imports
 from random import randint, choice as rc
 
+# Remote library imports
 from faker import Faker
 
+# Local imports
 from app import app
-from models import db, Activity, Signup, Camper
-
-fake = Faker()
-
-
-def create_activities():
-    activities = []
-    for _ in range(10):
-        a = Activity(
-            name=fake.sentence(),
-            difficulty=randint(1, 5)
-        )
-        activities.append(a)
-
-    return activities
-
-
-def create_campers():
-    campers = []
-    for _ in range(5):
-        c = Camper(
-            name=fake.name(),
-            age=rc(range(8, 19))
-        )
-        campers.append(c)
-
-    return campers
-
-
-def create_signups(activities, campers):
-    signups = []
-    for _ in range(20):
-        s = Signup(
-            time=rc(range(24)),
-            camper_id=rc([camper.id for camper in campers]),
-            activity_id=rc([activity.id for activity in activities])
-        )
-        signups.append(s)
-
-    return signups
-
+from models import db
 
 if __name__ == '__main__':
-
+    fake = Faker()
     with app.app_context():
-        print("Clearing db...")
-        Activity.query.delete()
-        Signup.query.delete()
-        Camper.query.delete()
-
-        print("Seeding activities...")
-        activities = create_activities()
-        db.session.add_all(activities)
-        db.session.commit()
-
-        print("Seeding campers...")
-        campers = create_campers()
-        db.session.add_all(campers)
-        db.session.commit()
-
-        print("Seeding signups...")
-        signups = create_signups(activities, campers)
-        db.session.add_all(signups)
-        db.session.commit()
-
-        print("Done seeding!")
+        print("Starting seed...")
+        # Seed code goes here!
